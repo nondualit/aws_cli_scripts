9a42dd45ea36fd0d3e8b50fff244a0f8f7835893
nondualit <anibal@nondualit.nl>
2019-05-30 22:27:05 +0200

    Create ec2_aws_firewall.sh

---
+++ ec2_aws_firewall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+################################################################################
+# Author    : Anibal Ojeda
+# Version   : 1.0
+# Date      : 29-05-2019
+# Description: Add or delete ip from AWS firewall 
+# More info https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html
+################################################################################
+aws ec2 describe-security-groups --group-ids | grep GroupName
+echo "Enter GroupName: "
+read var1
+
+myip=`curl https://ipinfo.io/ip`
+
+while [ -n "$1" ]; do # while loop starts
+
+    case "$1" in
+ 
+    -a)  aws ec2 authorize-security-group-ingress --group-name $var1 --protocol tcp --port 22 --cidr $myip/24 ;; # add option
+ 
+    -d)  aws ec2 revoke-security-group-ingress --group-name $var1 --protocol tcp --port 22 --cidr $myip/24 ;; # del option
+ 
+ 
+    *) echo "Option $1 not recognized" ;; # In case you typed a different option other than a,d
+ 
+    esac
+ 
+    shift
+ 
+done
+
+aws ec2 describe-security-groups --group-name $var1 | grep 24
