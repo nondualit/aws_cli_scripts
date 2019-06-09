52c5770e44f4116a7b4678444105fd01884cfbb8
Anibal Enrique Ojeda <anibal@nondualit.nl>
2019-06-09 11:53:02 +0200

    commit
    commit

--- ec2_aws_firewall.sh
+++ ec2_aws_firewall.sh
@@ -2,7 +2,7 @@
 ################################################################################
 # Author    : Anibal Enrique Ojeda Gonzalez
 # url       : www.nondualit.nl
-# Version   : 1.0
+# Version   : 1.1
 # Date      : 30-05-2019
 # Description: Add or delete ip from AWS ec2 VPC Security Group firewall 
 # More info https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html
@@ -10,7 +10,13 @@
 # Get VPC Group Names
 aws ec2 describe-security-groups --group-ids | grep GroupName
 
-# Enter values
+
+# Options
+while [ -n "$1" ]; do # while loop starts
+
+    case "$1" in
+ 
+ # Enter values
 echo "Enter GroupName: "
 read var1
 echo "Enter port number: "
@@ -19,17 +25,22 @@ read var2
 # Get your local IP address
 myip=`curl https://ipinfo.io/ip`
 
-
-# Options
-while [ -n "$1" ]; do # while loop starts
-
-    case "$1" in
- 
     -a)  aws ec2 authorize-security-group-ingress --group-name $var1 --protocol tcp --port $var2 --cidr $myip/24 ;; # add option
  
+ # Enter values
+echo "Enter GroupName: "
+read var1
+echo "Enter port number: "
+read var2
+
+# Get your local IP address
+myip=`curl https://ipinfo.io/ip`
+
     -d)  aws ec2 revoke-security-group-ingress --group-name $var1 --protocol tcp --port $var2 --cidr $myip/24 ;; # del option
     
-    -h)  echo "use -a to add your IP address to your AWS ec2 SecurityGroup or -d to delete your IP address from the AWS SecurityGroup" ;; # Help option
+    -h)  echo "use -a to add your IP address to your AWS ec2 SecurityGroup, -s to show IP address or -d to delete your IP address from the AWS SecurityGroup" ;; # Help option
+
+    -s)  aws ec2 describe-security-groups --group-name $var1 | grep 24 ;; show ip 
 
      *) echo "Option $1 not recognized. Use -h for help" ;; # In case you typed a different option other than a,d,h
  
@@ -40,4 +51,4 @@ while [ -n "$1" ]; do # while loop starts
 done
 
 # Show IP added to VPC Securty Group.
-aws ec2 describe-security-groups --group-name $var1 | grep 24
+#aws ec2 describe-security-groups --group-name $var1 | grep 24
